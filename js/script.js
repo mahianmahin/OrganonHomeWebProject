333929e10a432703e68703fbd70e745ac9c9ae37
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2021-08-25 23:33:55 +0600

    navbar & phone navbar done

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+if (window.outerWidth < 1024) {
+    document.getElementById('logo_img').src = "../img/logo/logo_main.png";
+};
+
+document.getElementById('toggle_btn').addEventListener('click', () => {
+    document.getElementById('phone_nav').style.right = 0;
+    document.getElementById('overlay').style.display = "block";
+});
+
+document.getElementById('back').addEventListener('click', () => {
+    document.getElementById('phone_nav').style.right = "-150%";
+    document.getElementById('overlay').style.display = "none";
+});
